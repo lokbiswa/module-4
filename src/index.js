08384f52f2f7a64926871106689a1a6cc7626d57
Lok Shankar <lokbiswa@gmail.com>
2021-09-02 15:29:35 -0500

    added password exercise

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// for (i = 0; i < 10; i++) {
+//   document.body.innerHTML += `<p>${i}</p>`;
+// }
+
+const ans = "Password";
+let user = "";
+while (user != ans) {
+  user = prompt("Enter your Password");
+  if (user === ans) {
+    alert("Welcome to the program");
+  }
+}
